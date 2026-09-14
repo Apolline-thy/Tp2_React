f1e5bd646219c119dda5c4db3473ebdffc37986d
Apolline-thy <apolline.thierry@etu.unilim.fr>
2026-09-14 17:55:23 +0200

    Card avancée

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,17 @@
-function Card (user, followers, platform, today) {
-    return(
-
-    )
+function Card(user, followers, icon, today) {
+  return (
+    <div>
+      <div className="flex flex-row items-center">
+        <img src={icon} alt={user} />
+        <h2>{user}</h2>
+      </div>
+      <div>
+        <h1>{followers}</h1>
+        <h2>FOLLOWERS</h2>
+      </div>
+      <p>{today}</p>
+    </div>
+  );
 }
+
+export default Card;
